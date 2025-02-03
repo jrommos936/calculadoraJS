5888df52449a5685c2a860c0e77c87669851f33f
jrommos936 <jrommos936@g.educaand.es>
2025-02-03 11:23:04 +0100

    Añadir nuevas cuentas

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,21 @@
-function sumar(a, b){
+function sumar(a, b) {
     return a + b;
-}
-
-function restar(a, b) {
+  }
+  
+  function restar(a, b) {
     return a - b;
-}
+  }
 
-module.exports= {sumar, restar};
+  function multiplicar(a, b){
+    return a * b; 
+  }
 
+  function dividir(a, b){
+    return a/b;
+  }
+  
+  function raizCuadrada(a){
+    return Math.sqrt(a);
+  }
+  
+  module.exports = { sumar, restar, multiplicar, dividir, raizCuadrada };
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,4 +1,4 @@
-const { sumar, restar } = require('./calculadora');
+const { sumar, restar, multiplicar, dividir, raizCuadrada } = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
     expect(sumar(2, 3)).toBe(5);
@@ -6,4 +6,16 @@ test('Suma 2 + 3 y devuelve 5', () => {
 
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
+});
+
+test('multiplicar 5 * 2 y devuelve 10', () => {
+    expect(multiplicar(5,2)).toBe(10);
+});
+
+test('dividir 10 / 5 y devuelve 2', () => {
+    expect(dividir(10,5)).toBe(2);
+});
+
+test('Raiz cuadrada de 16 y devuelve 4', () => {
+    expect(raizCuadrada(16)).toBe(4);
 });
