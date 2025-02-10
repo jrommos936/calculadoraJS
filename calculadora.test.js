889c74c94af30214ef093bd54817cf2775e1d168
jrommos936 <jrommos936@g.educaand.es>
2025-02-10 11:16:46 +0100

    Nueva calculadora la verdadera corregida

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,11 +1,11 @@
-const { sumar, restar, multiplicar, dividir, raizCuadrada } = require('./calculadora');
+const { suma, resta, multiplicar, dividir} = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
-    expect(sumar(2, 3)).toBe(5);
+    expect(suma(2, 3)).toBe(5);
 });
 
 test('Resta 5 - 3 y devuelve 2', () => {
-    expect(restar(5, 3)).toBe(2);
+    expect(resta(5, 3)).toBe(2);
 });
 
 test('multiplicar 5 * 2 y devuelve 10', () => {
